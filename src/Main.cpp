da91902b8ef1ae5adaca8d2ecd222d1542d8dfa4
Linus J <linus.jansson2000@gmail.com>
2023-01-17 21:13:57 +0100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,15 @@
 #include <Utils.h>
 #include <User.h>
 
+void init() {
+    std::cout << "Initializing..." << std::endl;
+    std::cout << "Done!" << std::endl;
+}
+
 void loop() {
+    // Initialize program
+    init();
+
     std::cout << User::Monitor::getMonitorResolution().x << " " << User::Monitor::getMonitorResolution().y << std::endl;
     std::cin.get();
     while (true) {
@@ -18,12 +26,7 @@ void loop() {
 }
 
 int main() {
-    try {
-        std::cout << "hello world" << std::endl;
-        loop();
-        return 0;
-    }
-    catch {
-        return 1;
-    }
+    std::cout << "hello world" << std::endl;
+    loop();
+    return 0;
 }
