391992f22868c74dd8e9d8c2c2f577e51d3a4e9f
Linus J <linus.jansson2000@gmail.com>
2023-01-17 21:11:08 +0100

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,9 +2,7 @@
 #include <Utils.h>
 #include <User.h>
 
-int main() {
-    std::cout << "hello world" << std::endl;
-
+void loop() {
     std::cout << User::Monitor::getMonitorResolution().x << " " << User::Monitor::getMonitorResolution().y << std::endl;
     std::cin.get();
     while (true) {
@@ -12,12 +10,20 @@ int main() {
 
         std::cout << mousePos.x << " " << mousePos.y << std::endl;
 
-
         // Failsafe
         if (mousePos.x == 0 && mousePos.y == 0) {
             break;
         }
     }
+}
 
-    return 0;
+int main() {
+    try {
+        std::cout << "hello world" << std::endl;
+        loop();
+        return 0;
+    }
+    catch {
+        return 1;
+    }
 }
